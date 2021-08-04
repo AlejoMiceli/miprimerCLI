b7496ce1d98b0cf4da3e91b9d22eee23ca52f1b6
Alejo <micelialejo@gmail.com>
2021-08-04 03:52:01 -0300

    realicé la actividad en el carrito

--- src/componentes/Cart.js
+++ src/componentes/Cart.js
@@ -1,11 +1,32 @@
 import React from "react"
 import context from "./CartContext"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 
 export default function Carrito () {
 
 
-    const  {carrito} = useContext (context)
+    const  {carrito, setCarrito, setCartCount} = useContext (context)
+    const [precioTotal,  setPrecioTotal] = useState(0)
+    function vaciarCarrito (){
+        setCarrito([])
+        setCartCount(0)
+    }
+
+    function removerProducto(id){
+        let filtro = carrito.filter((x) => x.item.id != id)
+        setCarrito(filtro)
+    }
+
+    function precioFinal(){
+        let total = 0
+        carrito.map ((pf)=>{
+        total = total + pf.item.price*pf.cantidad
+         }
+        
+        )
+        setPrecioTotal(total)
+    }
+
     console.log(carrito)
     return (
         <>     
@@ -16,12 +37,16 @@ export default function Carrito () {
                 {carrito.map((e) =>  ( 
                   <div> 
                       <h1>{e.item.title}</h1> 
-                      <h1>{e.cantidad}</h1>                   
+                      <h1>{e.cantidad}</h1> 
+                      <h1>${e.item.price}</h1> ={e.cantidad*e.item.price}
+                      <button onClick={() => removerProducto (e.item.id)}>Quitar producto</button>                  
                      </div>
                      
                     )
                 )}
             </div>
+            <button onClick={precioFinal}>Calcular Total</button>
+            <h1>{precioTotal}</h1>
             
         </>
     )
