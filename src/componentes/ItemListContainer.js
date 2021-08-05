dc1e5a47e1870cc75c5ff69e3d1a1f835d151767
Alejo Miceli <L0307696@bancogalicia.com.ar>
2021-08-05 02:10:11 -0300

    firebase

--- src/componentes/ItemListContainer.js
+++ src/componentes/ItemListContainer.js
@@ -1,11 +1,36 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import Item from "./Item"
+import { getFirestore } from "../firebase"
 
 export default function Contenedor (){
     
     const [categorias, setCategorias] = useState ([])
     const { categoryId } = useParams ();
+
+    const [producto, setProducto] = useState ({})
+    useEffect (()=> {
+       const firestore = getFirestore() 
+       const collection = firestore.collection("productos")
+       const query = collection.get()
+            query
+                .then((snapshot) => {
+                    const documentos = snapshot.docs
+                    const producto = documentos.map((doc) => {
+                        return { id: doc.id, ...doc.data() }
+                    })
+
+                    setTimeout(() => {
+                        
+                        setProducto(producto)
+                    }, 2000)
+
+
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+    })
 
     const config = {
         method: 'GET',
@@ -41,6 +66,7 @@ useEffect ( ()=> {
 
 return (
     <>
+    {/* <h1>{producto[0].title}</h1> */}
     <h1 className="titulo">Productos</h1>
     <div className="card_container">
         {categorias.map((item) =>
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBcVYnWO6Xm9Qziizbsutw1EKUrBfgfFSk",
+    authDomain: "mi-primer-e-commerce.firebaseapp.com",
+    projectId: "mi-primer-e-commerce",
+    storageBucket: "mi-primer-e-commerce.appspot.com",
+    messagingSenderId: "15774373858",
+    appId: "1:15774373858:web:02a5d050b6ea7789c98bbb"
+  };
+  
+  const app = firebase.initializeApp(firebaseConfig);
+
+  export const getFirestore = () => {
+
+    return firebase.firestore(app)
+  }
